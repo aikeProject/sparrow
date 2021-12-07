e4817871dcbe2964f6889e7ae3e53d9f0b935ef4
chengyu <rensailong259@hellobike.com>
2021-12-07 19:42:25 +0800

    feat: eslint jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    // 这里添加一行规则把这条规则隐藏
+    'import/prefer-default-export': 0,
+  },
 };
